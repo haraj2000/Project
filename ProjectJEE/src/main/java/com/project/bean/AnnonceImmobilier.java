baa8817e22677b9a13cce4a2fa0a3763ba7b4315
blackswan <blackswan@192.168.1.18>
2020-02-21 11:03:45 +0000

    beans annonceImmobilier

---
+++ ProjectJEE/src/main/java/com/project/bean/AnnonceImmobilier.java
@@ -0,0 +1,15 @@
+package com.project.bean;
+
+import java.sql.Date;
+
+public class AnnonceImmobilier {
+	private int id;
+	private float prix;
+	private float surface;
+	private String titreAnnonce;
+	private Date dateAnnonce;
+	private String adresseImm;
+	private String villeImm;
+	
+
+}
