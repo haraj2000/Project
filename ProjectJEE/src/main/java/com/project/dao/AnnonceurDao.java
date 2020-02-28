ea664f82addb037869ad4634275faf794bc54bce
Haraj <Haraj@Haraj>
2020-02-28 16:24:39 +0100

    dao works now

--- ProjectJEE/src/main/java/com/project/dao/AnnonceurDao.java
+++ ProjectJEE/src/main/java/com/project/dao/AnnonceurDao.java
@@ -3,10 +3,10 @@ package com.project.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.project.bean.User;
+import com.project.bean.Annonceur;
 
 @Repository
-public interface UserDao extends JpaRepository<User, Long> {
+public interface AnnonceurDao extends JpaRepository<Annonceur, Long> {
 	
 
 }
