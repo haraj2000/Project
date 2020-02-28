16667014b5a9a75475787c07b715910123e124e7
Haraj <Haraj@Haraj>
2020-02-28 16:01:03 +0100

    getter and setter annonceur

--- ProjectJEE/src/main/java/com/project/bean/Annonceur.java
+++ ProjectJEE/src/main/java/com/project/bean/Annonceur.java
@@ -1,5 +1,7 @@
 package com.project.bean;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -24,9 +26,139 @@ public class Annonceur {
 	@ManyToOne
 	private TypeAnnonceur typeAnnonceur;
 	@OneToMany
-	private AnnonceImmobilier annonceImmobilier;
+	private List<AnnonceImmobilier> annonceImmobilier;
 	@OneToMany
-	private AnnonceVoiture annonceVoiture;
+	private List<AnnonceVoiture> annonceVoiture;
 	@OneToMany
-	private Commentaire commentaire;
+	private List<Commentaire> commentaire;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return Last_name;
+	}
+	public void setLast_name(String last_name) {
+		Last_name = last_name;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public int getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(int phone_number) {
+		this.phone_number = phone_number;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public TypeAnnonceur getTypeAnnonceur() {
+		return typeAnnonceur;
+	}
+	public void setTypeAnnonceur(TypeAnnonceur typeAnnonceur) {
+		this.typeAnnonceur = typeAnnonceur;
+	}
+	public List<AnnonceImmobilier> getAnnonceImmobilier() {
+		return annonceImmobilier;
+	}
+	public void setAnnonceImmobilier(List<AnnonceImmobilier> annonceImmobilier) {
+		this.annonceImmobilier = annonceImmobilier;
+	}
+	public List<AnnonceVoiture> getAnnonceVoiture() {
+		return annonceVoiture;
+	}
+	public void setAnnonceVoiture(List<AnnonceVoiture> annonceVoiture) {
+		this.annonceVoiture = annonceVoiture;
+	}
+	public List<Commentaire> getCommentaire() {
+		return commentaire;
+	}
+	public void setCommentaire(List<Commentaire> commentaire) {
+		this.commentaire = commentaire;
+	}
+	
+	public Annonceur(long id, String login, String password, String first_name, String last_name, String mail,
+			int phone_number, String country, String city, String address, TypeAnnonceur typeAnnonceur,
+			List<AnnonceImmobilier> annonceImmobilier, List<AnnonceVoiture> annonceVoiture,
+			List<Commentaire> commentaire) {
+		super();
+		this.id = id;
+		this.login = login;
+		this.password = password;
+		this.first_name = first_name;
+		Last_name = last_name;
+		this.mail = mail;
+		this.phone_number = phone_number;
+		this.country = country;
+		this.city = city;
+		this.address = address;
+		this.typeAnnonceur = typeAnnonceur;
+		this.annonceImmobilier = annonceImmobilier;
+		this.annonceVoiture = annonceVoiture;
+		this.commentaire = commentaire;
+	}
+	
+	public Annonceur() {
+		super();
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Annonceur other = (Annonceur) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
 }
