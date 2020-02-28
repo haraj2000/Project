a4d0ed61c9d274feaac5d5d50ae08458d14c28b2
Nimus <Nimus@DESKTOP-KBK4HIN>
2020-02-28 16:22:48 +0100

    dea btissam

--- ProjectJEE/src/main/java/com/project/dao/MarqueDao.java
+++ ProjectJEE/src/main/java/com/project/dao/MarqueDao.java
@@ -1,12 +1,17 @@
 package com.project.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.project.bean.AnnonceVoiture;
 import com.project.bean.Marque;
 
 @Repository
 public interface MarqueDao extends JpaRepository<Marque, Long> {
-	
+	public Marque findByNom(String nom);
+
+	public List<Marque> findByModeleVersion(String version);
 
 }
