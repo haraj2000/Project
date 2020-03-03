2fb0a70cb9bb0ad8b3a8d5f00169bd66672d1f6c
Haraj <Haraj@Haraj>
2020-03-03 10:12:00 +0100

    AnnonceurDao

--- ProjectJEE/src/main/java/com/project/dao/AnnonceurDao.java
+++ ProjectJEE/src/main/java/com/project/dao/AnnonceurDao.java
@@ -1,5 +1,7 @@
 package com.project.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,8 @@ import com.project.bean.Annonceur;
 @Repository
 public interface AnnonceurDao extends JpaRepository<Annonceur, Long> {
 	
-
+	public Annonceur findByLogin(String login);
+	public List<Annonceur> findByLastName(String lastName);
+	public Annonceur findByMail(String mail);
+	
 }
--- ProjectJEE/src/main/java/com/project/ws/ReviewRest.java
+++
@@ -1,8 +0,0 @@
-package com.project.ws;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class ReviewRest {
-
-}
