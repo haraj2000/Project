6653c4066bbbaabf1d72e3c0e589bd09fafabdb5
lamya <lamya@DESKTOP-FBHM6H0>
2020-03-04 12:51:17 +0100

    update

--- ProjectJEE/src/main/java/com/project/service/façade/CarburantService.java
+++ ProjectJEE/src/main/java/com/project/service/façade/CarburantService.java
@@ -1,5 +1,15 @@
 package com.project.service.façade;
 
+import java.util.List;
+
+import com.project.bean.Carburant;
+
+
 public interface CarburantService {
+
+
+	public void save(Carburant annonceImmobilier);
+
+	public List<Carburant> findAll();
 
 }
--- ProjectJEE/src/main/java/com/project/service/impl/CarburantServiceImpl.java
+++ ProjectJEE/src/main/java/com/project/service/impl/CarburantServiceImpl.java
@@ -1,5 +1,26 @@
 package com.project.service.impl;
 
-public class CarburantServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.project.bean.Carburant;
+import com.project.dao.CarburantDao;
+import com.project.service.façade.CarburantService;
+
+public class CarburantServiceImpl implements CarburantService {
+	@Autowired
+	private CarburantDao carburantDao;
+
+
+	@Override
+	public void save(Carburant carburant) {
+		carburantDao.save(carburant);
+	}
+
+	@Override
+	public List<Carburant> findAll() {
+		return carburantDao.findAll() ;
+	}
 
 }
