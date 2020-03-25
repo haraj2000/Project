77a8b687770e00556176ac0d947403e05c007a09
Haraj <Haraj@192.168.59.1>
2020-03-25 20:42:03 +0100

    identify

--- ProjectJEE/src/main/java/com/project/bean/Annonceur.java
+++ ProjectJEE/src/main/java/com/project/bean/Annonceur.java
@@ -10,7 +10,7 @@ import javax.persistence.ManyToOne;
 @Entity
 public class Annonceur {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String mail;
 	private String password;
