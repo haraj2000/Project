8eb1d5c5ef9d8ddce1d03a68afa9e9057f7dbb72
lamya <lamya@DESKTOP-FBHM6H0>
2020-04-08 01:15:43 +0100

    rest

--- ProjectJEE/src/main/java/com/project/ws/TransmissionRest.java
+++ ProjectJEE/src/main/java/com/project/ws/TransmissionRest.java
@@ -3,6 +3,7 @@ package com.project.ws;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.project.bean.Transmission;
 import com.project.service.facade.TransmissionService;
 
 @RestController
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RequestMapping("project/Transmission")
 public class TransmissionRest {
 	@Autowired
